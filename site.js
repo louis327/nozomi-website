f7dce0eead2f4dc7f52867f9e83e9f952b2b8032
louis327 <louis@nozomi.network>
2026-02-08 20:49:02 +1100

    Update site.js to use favicon.png instead of SVG

--- site.js
+++ site.js
@@ -1,11 +1,17 @@
 // Nozomi shared site utilities
 (function(){
-  // Inject favicon
+  // Inject favicon (PNG)
   var link = document.createElement('link');
   link.rel = 'icon';
-  link.type = 'image/svg+xml';
-  link.href = '/favicon.svg';
+  link.type = 'image/png';
+  link.href = '/favicon.png';
   document.head.appendChild(link);
+
+  // Also set as apple-touch-icon
+  var apple = document.createElement('link');
+  apple.rel = 'apple-touch-icon';
+  apple.href = '/favicon.png';
+  document.head.appendChild(apple);
 
   // Fix old .html links to clean URLs
   document.querySelectorAll('a[href*="gtm.html"]').forEach(function(a){
